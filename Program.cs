105f737545eaad25878130c4da4a06add90589fb
Daniel Yoon <64057788+yd24@users.noreply.github.com>
2024-01-25 19:21:05 -0800

    Add practice console commands

--- Program.cs
+++ Program.cs
@@ -10,7 +10,15 @@ namespace PracticeApp
     {
         static void Main(string[] args)
         {
+            //Console.WriteLine("Hello World!");
+            string firstName = "Bob";
+            int number = 32;
+            float value = 5.32F;
 
+            Console.WriteLine($"This is the order for {firstName}. He ordered {number} cheeseburgers.");
+            Console.WriteLine($"His total comes to: ${value * number}.");
+            
+            Console.ReadLine();
         }
     }
 }
