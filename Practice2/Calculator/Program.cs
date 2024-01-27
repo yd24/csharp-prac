58b770cb0ad6bcf2eaf756fc964d97b6386b66b4
Daniel Yoon <64057788+yd24@users.noreply.github.com>
2024-01-26 20:11:46 -0800

    Initial commit for calculator CLI

---
+++ Practice2/Calculator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+
+namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Basic Calculator!");
+            Console.WriteLine("This calculator can perform addition, subtraction, multiplication, and division.");
+            Console.WriteLine();
+            Console.Write("Please enter your calculation: ");
+
+            string response = Convert.ToString(Console.ReadLine());
+        }
+    }
+}
