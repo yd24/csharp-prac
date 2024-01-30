76744fe0e0dabb64dc6599289f8fbe5f41245fdf
Daniel Yoon <64057788+yd24@users.noreply.github.com>
2024-01-29 19:39:27 -0800

    Implement basic integer Stack

---
+++ DataStructures/DataStructuresTest/IntStack.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructuresTest
+{
+    public class IntStack
+    {
+        public int MaxSize { get; set; }
+        public int[] StackArray { get; set; }
+        public int Top {  get; set; }
+        public IntStack(int size) {
+            this.MaxSize = size;
+            this.StackArray = new int[size];
+            this.Top = -1;
+        }
+
+        public void Push(int value)
+        {
+            this.Top++;
+            if (this.Top >= this.MaxSize)
+            {
+                this.MaxSize = this.MaxSize * 2;
+                int[] newArray = new int[this.MaxSize];
+                Array.Copy(StackArray, newArray, StackArray.Length);
+                this.StackArray = newArray;
+            }
+            this.StackArray[this.Top] = value;
+        }
+
+        public int Pop()
+        {
+            if (this.Top >= 0)
+            {
+                int val = this.StackArray[this.Top];
+                this.Top--;
+                return val;
+            }
+            return -1;
+        }
+
+        public int Peek()
+        {
+            if (this.Top < 0) 
+            {
+                return -1;
+            }
+            return this.StackArray[this.Top];
+        }
+
+        public int Count()
+        {
+            return this.StackArray.Length;
+        }
+
+    }
+}
---
+++ DataStructures/DataStructuresTest/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace DataStructuresTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //testing IntStack
+            var stack = new IntStack(10);
+            for (int i = 0; i < 10; i++)
+            {
+                stack.Push(i);
+                Console.WriteLine(stack.Peek());
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                int popped = stack.Pop();
+                Console.WriteLine($"{popped} is popped and {stack.Peek()} is next");
+            }
+
+            for (int i = 0; i < 20; i++)
+            {
+                stack.Push(i);
+            }
+
+            for (int i = 0; i <= stack.Count(); i++)
+            {
+                Console.WriteLine(stack.Pop());
+            }
+        }
+    }
+}
